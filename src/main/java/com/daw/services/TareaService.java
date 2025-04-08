ad9d862e9df232938e0a14a21de99bea1ed52edc
franvergara10 <vergarafran12345@gmail.com>
2025-04-08 13:23:13 +0200

    .

--- src/main/java/com/daw/services/TareaService.java
+++ src/main/java/com/daw/services/TareaService.java
@@ -2,6 +2,7 @@ package com.daw.services;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,51 +10,156 @@ import org.springframework.stereotype.Service;
 import com.daw.persistance.entities.Tarea;
 import com.daw.persistance.entities.enums.Estado;
 import com.daw.persistance.repositories.TareaRepository;
+import com.daw.services.exceptions.TareaException;
+import com.daw.services.exceptions.TareaNotFoundException;
 
 @Service
 public class TareaService {
-
+	
 	@Autowired
 	private TareaRepository tareaRepository;
-
-	public List<Tarea> finAll() {
+	
+	public List<Tarea> findAll(){
 		return this.tareaRepository.findAll();
-
 	}
 	
 	public Tarea findById(int idTarea) {
+		if(!this.tareaRepository.existsById(idTarea)) {
+			throw new TareaNotFoundException("No existe la tarea con ID: " + idTarea);
+		}
 		
-		return this.tareaRepository.findById(idTarea).get();	
+		return this.tareaRepository.findById(idTarea).get();
 	}
 	
-	public boolean existById(int idTarea) {
+	public boolean existsById(int idTarea) {
 		return this.tareaRepository.existsById(idTarea);
 	}
 	
-	public boolean deleteById(int idTarea) {
-		boolean result=false;
-		if(this.tareaRepository.existsById(idTarea)) {
-			this.tareaRepository.deleteById(idTarea);
-			result=true;
-		}
-		return result;
-	}
-	
-	public Tarea create (Tarea tarea) {
+	public Tarea create(Tarea tarea) {	
+		tarea.setId(0);
 		tarea.setFechaCreacion(LocalDate.now());
 		tarea.setEstado(Estado.PENDIENTE);
 		
 		return this.tareaRepository.save(tarea);
 	}
 	
-	public Tarea update (Tarea tarea) {
+	public Tarea update(int idTarea, Tarea tarea) {
+		if(idTarea != tarea.getId()) {
+			throw new TareaException("El ID del path ("+ idTarea +") y el id del body ("+ tarea.getId() +") no coinciden");
+		}
+		if(!this.tareaRepository.existsById(idTarea)) {
+			throw new TareaNotFoundException("No existe la tarea con ID: " + idTarea);
+		}
+		if(tarea.getFechaCreacion() != null || tarea.getEstado() != null) {
+			throw new TareaException("No se permite modificar la fecha de creación y/o el estado. ");
+		}
+		
 		Tarea tareaBD = this.findById(tarea.getId());
-		tarea.setFechaCreacion(tareaBD.getFechaCreacion());
-		tarea.setEstado(tareaBD.getEstado());
+		tareaBD.setTitulo(tarea.getTitulo());
+		tareaBD.setDescripcion(tarea.getDescripcion());
+		tareaBD.setFechaVencimiento(tarea.getFechaVencimiento());
 		
-		return this.tareaRepository.save(tarea);
-		
+		return this.tareaRepository.save(tareaBD);
 	}
 	
+	public void deleteById(int idTarea) {
+		if(!this.tareaRepository.existsById(idTarea)) {
+			throw new TareaNotFoundException("No existe la tarea con ID: " + idTarea);
+		}
+		
+		this.tareaRepository.deleteById(idTarea);
+	}
+	
+	
+	//Ejemplos Optional
+	public boolean deleteDeclarativo(int idTarea) {
+		boolean result = false;
+		
+		if(this.tareaRepository.existsById(idTarea)) {
+			this.tareaRepository.deleteById(idTarea);
+			result = true;
+		}
+		
+		return result;
+	}
+	
+	public boolean deleteFuncional(int idTarea) {
+		return this.tareaRepository.findById(idTarea)
+				.map(t -> {
+					this.tareaRepository.deleteById(idTarea);
+					return true;
+				})
+				.orElse(false);
+	}
+	
+	public Tarea findByIdFuncional(int idTarea) {		
+		return this.tareaRepository.findById(idTarea)
+				.orElseThrow(() -> new TareaNotFoundException("No existe la tarea con ID: " + idTarea));
+	}
+	
+	//Ejemplos Stream
+	//Obtener el número total de tareas completadas.
+	public long totalTareasCompletadasFuncional() {
+		return this.tareaRepository.findAll().stream()
+				.filter(t -> t.getEstado() == Estado.COMPLETADA)
+				.count();
+	}
+	
+	public long totalTareasCompletadas() {
+		return this.tareaRepository.countByEstado(Estado.COMPLETADA);
+	}
+	
+	//Obtener una lista de las fechas de vencimiento de las tareas que estén en progreso.
+	public List<LocalDate> fechasVencimientoEnProgresoFuncional() {
+		return this.tareaRepository.findAll().stream()
+				.filter(t -> t.getEstado() == Estado.EN_PROGRESO)
+				.map(t -> t.getFechaVencimiento())
+				.collect(Collectors.toList());
+	}
+	
+	public List<LocalDate> fechasVencimientoEnProgreso() {
+		return this.tareaRepository.findByEstado(Estado.EN_PROGRESO).stream()
+				.map(t -> t.getFechaVencimiento())
+				.collect(Collectors.toList());
+	}
+	
+	
+	//Obtener las tareas vencidas.
+	public List<Tarea> tareasVencidasFuncional() {
+		return this.tareaRepository.findAll().stream()
+				.filter(t -> t.getFechaVencimiento().isBefore(LocalDate.now()))
+				.collect(Collectors.toList());
+	}
+	
+	public List<Tarea> tareasVencidas() {
+		return this.tareaRepository.findByFechaVencimientoBefore(LocalDate.now());
+	}
+	
+	
+	//Obtener los títulos de las tareas pendientes.
+	public List<String> titulosPendientesFuncional() {
+		return this.tareaRepository.findAll().stream()
+				.filter(t -> t.getEstado() == Estado.PENDIENTE)
+				.map(t -> t.getTitulo())
+				.collect(Collectors.toList());
+	}
+	
+	public List<String> titulosPendientes() {
+		return this.tareaRepository.findByEstado(Estado.PENDIENTE).stream()
+				.map(t -> t.getTitulo())
+				.collect(Collectors.toList());
+	}
+	
+	
+	//Obtener las tareas ordenadas por fecha de vencimiento.
+	public List<Tarea> ordenadasFechaVencimientoFuncional(){
+		return this.tareaRepository.findAll().stream()
+				.sorted((t1, t2) -> t1.getFechaVencimiento().compareTo(t2.getFechaVencimiento()))
+				.collect(Collectors.toList());
+	}	
+
+	public List<Tarea> ordenadasFechaVencimiento(){
+		return this.tareaRepository.findAllByOrderByFechaVencimiento();
+	}
 
 }
--- src/main/java/com/daw/web/controllers/TareaController.java
+++ src/main/java/com/daw/web/controllers/TareaController.java
@@ -3,6 +3,8 @@ package com.daw.web.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.daw.persistance.entities.Tarea;
 import com.daw.services.TareaService;
+import com.daw.services.exceptions.TareaException;
+import com.daw.services.exceptions.TareaNotFoundException;
 
 @RestController
 @RequestMapping("/tareas")
@@ -23,27 +27,59 @@ public class TareaController {
 	private TareaService tareaService;
 
 	@GetMapping
-	public List<Tarea> list() {
+	public ResponseEntity<List<Tarea>> list() {
+		return ResponseEntity.status(HttpStatus.OK).body(this.tareaService.findAll());
+	}
 
-		return this.tareaService.finAll();
-	}
 	@GetMapping("/{idTarea}")
-	public Tarea findById(@PathVariable int idTarea) {
-		return this.tareaService.findById(idTarea);
+	public ResponseEntity<?> findById(@PathVariable int idTarea) {
+		try {
+			return ResponseEntity.ok(this.tareaService.findById(idTarea));
+		} catch (TareaNotFoundException ex) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+		}
 	}
-	
+
 	@DeleteMapping("/{idTarea}")
-	public boolean delete(@PathVariable int idTarea) {
-		return this.tareaService.deleteById(idTarea);
+	public ResponseEntity<?> delete(@PathVariable int idTarea) {
+		try {
+			this.tareaService.deleteById(idTarea);
+			return ResponseEntity.ok("La tarea con ID("+ idTarea +") ha sido borrada correctamente. " );
+		} catch (TareaNotFoundException ex) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+		}
+
 	}
-	
+
 	@PostMapping
-	public Tarea create(@RequestBody Tarea tarea) {
-		return this.tareaService.create(tarea);
+	public ResponseEntity<Tarea> create(@RequestBody Tarea tarea) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(this.tareaService.create(tarea));
+	}
+
+	@PutMapping("/{idTarea}")
+	public ResponseEntity<?> update(@PathVariable int idTarea, @RequestBody Tarea tarea) {
+		try {
+			return ResponseEntity.ok(this.tareaService.update(idTarea, tarea));
+		} catch (TareaNotFoundException ex) {
+			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+		} catch (TareaException ex) {
+			return ResponseEntity.status(HttpStatus.CONFLICT).body(ex.getMessage());
+		}
 	}
 	
-	@PutMapping("/{idTarea}")
-	public Tarea update (@PathVariable int idTarea, @RequestBody Tarea tarea) {
-		return this.tareaService.update(tarea);
-	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
 }
